632e14572d1e1c449ef8886a23c371008cc773e6
Walison Teodoro <theodoro.walison@gmail.com>
2021-06-22 18:05:12 -0300

    aula 2

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
+import 'reflect-metadata'
 import express from 'express'
+import { router } from './routes'
+
+import './database'
 
 const app = express()
 
-app.get('/test', (request, response) => {
-  return response.send('Olá Walison')
-})
+app.use(express.json())
 
-app.post('/test-post', (request, response) => {
-  return response.send('Olá Walison, esse é o método post')
-})
+app.use(router)
 
 // http://localhost:3000
 app.listen(3000, () => console.log('Server is running!')) 
